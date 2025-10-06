b27b64c0bfc4c1d77404dc0fda412a0028637e5f
hi-yiyang <51028862+xuintl@users.noreply.github.com>
2025-10-06 13:20:26 +0400

    fff

--- IntroIM/synthestry-server/index.js
+++ IntroIM/synthestry-server/index.js
@@ -85,3 +85,5 @@ app.post('/generate-image', async (req, res) => {
 app.listen(port, () => {
   console.log(`Synthestry server listening on port ${port}`);
 });
+
+// don't you fcking see this change????????
